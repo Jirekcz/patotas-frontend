4f13d47d518cde97d51be52f755185f3bc8e2f37
Jireh Flores <Jirekcz03ft@gmail.com>
2024-10-13 15:46:19 -0500

    Configuracion de TimeOut con FeignClient

--- src/main/java/pe/edu/cibertec/Patotas_frontend/client/AutenticacionClient.java
+++ src/main/java/pe/edu/cibertec/Patotas_frontend/client/AutenticacionClient.java
@@ -4,10 +4,11 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import pe.edu.cibertec.Patotas_frontend.config.FeignClientConfig;
 import pe.edu.cibertec.Patotas_frontend.dto.LoginRequestDTO;
 import pe.edu.cibertec.Patotas_frontend.dto.LoginResponseDTO;
 
-@FeignClient(name = "autenticacion", url = "http://localhost:8081/autenticacion")
+@FeignClient(name = "autenticacion", url = "http://localhost:8081/autenticacion", configuration = FeignClientConfig.class)
 public interface AutenticacionClient {
     @PostMapping("/login")
     ResponseEntity<LoginResponseDTO> login(@RequestBody LoginRequestDTO loginRequestDTO);
